184a04b3819981fbb393169100a14f04963a506b
redmejia <preynaldorod@gmail.com>
2021-07-07 22:10:53 -0700

    listen for go routins connection and broadcast msg

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,12 +1,17 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/CloudyKit/jet/v6"
 	"github.com/gorilla/websocket"
 )
+
+var wsChan = make(chan WsPayload)
+
+var clients = make(map[WebSocketConnection]string)
 
 var views = jet.NewSet(
 	jet.NewOSFileSystemLoader("./html"),
@@ -54,17 +59,60 @@ func WsEndPoint(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 
-	log.Println("cliet connected to encpoint ")
+	log.Println("client connected to encpoint ")
 
 	var response WsJsonResp
 	response.Message = `<em><small>connected to serv</small></em>`
+
+	conn := WebSocketConnection{Conn: ws}
+	clients[conn] = ""
 
 	err = ws.WriteJSON(response)
 	if err != nil {
 		log.Println(err)
 	}
-}
 
+	go ListenForWs(&conn)
+}
+func ListenForWs(conn *WebSocketConnection) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Error", fmt.Sprintf("%v", r))
+		}
+	}()
+
+	var payload WsPayload
+
+	for {
+		err := conn.ReadJSON(&payload)
+		if err != nil {
+			//
+		} else {
+			payload.Conn = *conn
+			wsChan <- payload
+		}
+	}
+}
+func ListenToWsChannel() {
+	var response WsJsonResp
+	for {
+		e := <-wsChan
+
+		response.Action = "Got Here"
+		response.Message = fmt.Sprintf("Some message %s", e.Action)
+		bradcastToAll(response)
+	}
+}
+func bradcastToAll(response WsJsonResp) {
+	for client := range clients {
+		err := client.WriteJSON(response)
+		if err != nil {
+			log.Println("Websocket err")
+			_ = client.Close()
+			delete(clients, client)
+		}
+	}
+}
 func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
 	view, err := views.GetTemplate(tmpl)
 	if err != nil {
--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/wschat/handlers"
 )
 
 func main() {
 	mux := routes()
+	log.Println("stating channel listner")
+	go handlers.ListenToWsChannel()
 	fmt.Println("Serv is started at http://127.0.0.1:8080/")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
