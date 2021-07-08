c617ff57c518018b3a553ecb954bfabb5dd25572
redmejia <preynaldorod@gmail.com>
2021-07-08 15:31:14 -0700

    websocket reconnecting and static files

--- routes.go
+++ routes.go
@@ -11,5 +11,8 @@ func routes() http.Handler {
 	mux := pat.New()
 	mux.Get("/", http.HandlerFunc(handlers.Home))
 	mux.Get("/ws", http.HandlerFunc(handlers.WsEndPoint))
+
+	fileSrv := http.FileServer(http.Dir("./static/"))
+	mux.Get("/static/", http.StripPrefix("/static", fileSrv))
 	return mux
 }
